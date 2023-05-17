dde21d8942717e154cb421497ca8c8cbeaf6cff7
Marina J Villalta Cerezo <marinavcsg@gmail.com>
2023-05-17 14:19:34 +0200

    Add files via upload

---
+++ prank.ps1
@@ -0,0 +1,36 @@
+$skull = @"
+                        888888888888888888888888888888888888888888888888888888888888
+                        888888888888888888888888888888888888888888888888888888888888
+                        8888888888888888888888888P""  ""9888888888888888888888888888
+                        8888888888888888P"88888P          988888"9888888888888888888
+                        8888888888888888  "9888            888P"  888888888888888888
+                        888888888888888888bo "9  d8o  o8b  P" od88888888888888888888
+                        888888888888888888888bob 98"  "8P dod88888888888888888888888
+                        888888888888888888888888    db    88888888888888888888888888
+                        88888888888888888888888888      8888888888888888888888888888
+                        88888888888888888888888P"9bo  odP"98888888888888888888888888
+                        88888888888888888888P" od88888888bo "98888888888888888888888
+                        888888888888888888   d88888888888888b   88888888888888888888
+                        8888888888888888888oo8888888888888888oo888888888888888888888
+                        888888888888888888888888888888888888888888888888888888888888
+"@
+
+Write-Host $skull -ForegroundColor Red
+
+echo "`n"
+echo "`n"
+. D:\Users\carlos.deprada\Downloads\texto.ps1 -FontColor red -Text 'Has sido hackeado' 
+echo "`n"
+echo "`n"
+. D:\Users\carlos.deprada\Downloads\texto.ps1 -FontColor Cyan -Text '  STRATESYS'
+
+echo "Javier Castro Bravo`n"
+echo "Nicolás Mendoza`n"
+echo "Juan Manzano Barea`n"
+echo "Carlos Ruiz de la Prada`n"
+echo "Román García Cruzado`n"
+echo "Antonio Cebreiro Bernárdez `n"
+echo "Jose Javier Cantero Otero`n"
+echo "Marina Jiali Villalta Cerezo`n"
+echo "Laura Mabel Rueda`n"
+echo "José Pablo Lemus Romero`n"
